bc6d6e1372c17e78c27ea983ada2a0bbe5ad943e
Hallar06 <hallarahmed.narejo@gmail.com>
2023-01-15 16:57:09 +0500

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+const Color redAccentclr = Colors.redAccent;
+const Color Greenclr = Colors.green;
+const Color RedClr = Colors.red;
+const Color YellowClr = Colors.yellow;
+
+const headingTextStyle = TextStyle(
+  fontSize: 40,
+  color: Colors.teal,
+  fontWeight: FontWeight.bold,
+);
